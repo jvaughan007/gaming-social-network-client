67741cf5f4a0162588b3cbe4fbc0bbba84a2b995
Donovan <donovanle18@gmail.com>
2020-12-24 13:31:46 -0600

    landing page mockup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,55 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import NavBar from "./NavBar/NavBar";
+import backImg from "./images/prple-controller.jpg";
 
 const Home = () => {
-  return (
-    <StyledMain>
-      <h1>Gaming Social Network</h1>
-    </StyledMain>
-  );
+    return (
+        <StyledMain>
+            <div className='landing-page'>
+                <div>
+                    <nav>
+                        <NavBar />
+                    </nav>
+                </div>
+                <div className='title-motto'>
+                    <h1>Gaming Social Network</h1>
+                    <span>Connecting Gamers since 2077</span>
+                </div>
+            </div>
+        </StyledMain>
+    );
 };
 
 const StyledMain = styled.main`
-  h1 {
-    text-align: center;
-  }
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    body {
+    }
+    .title-motto {
+        padding: 17rem 0;
+        text-align: center;
+        margin: auto;
+    }
+    h1 {
+        color: #14ffec;
+        padding-bottom: 0.8rem;
+    }
+    span {
+        color: white;
+        font-size: 1.9rem;
+    }
+    .landing-page {
+        background-image: url(${backImg});
+        background-size: cover;
+        background-position: -80rem -8rem;
+        height: 90rem;
+        padding-bottom: 0;
+        width: 100%;
+        overflow-y: hidden;
+        position: relative;
+    }
 `;
 
 export default Home;
