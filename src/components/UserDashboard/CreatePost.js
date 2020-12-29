04f5dbe7f950a6755d83d03be531a366c0a824df
Dariss Harris <darissharris@gmail.com>
2020-12-29 10:32:15 -0800

    added workable code for create post component

--- src/components/UserDashboard/CreatePost.js
+++ src/components/UserDashboard/CreatePost.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
 
 class CreatePost extends Component {
-  //simple form with 
+  constructor(props){
+    super(props);
+    this.state = {
+      content: "",
+      youtube_url: "",
+      image_url: "",
+      public: "",
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {
+      content,
+      youtube_url,
+      image_url,
+      public,
+    } = e.target;
+    this.setState({ error: null });
+    PostsApiService.postArticle({
+      content: content.value,
+      youtube_url: youtube_url.value,
+      image_url: image_url.value,
+      public: public.value,
+    })
+    .then((user) => {
+      content.value = "";
+      youtube_url.value = "";
+      image_url.value = "";
+      public.value = "";
+      TokenService.saveAuthToken(user.authToken);
+      this.props.history.push("/userdashboard");
+    })
+    .catch((res) => {
+      this.setState({ error: res.error });
+    });
+  }
+
+  handleChange = (evt) => {
+    evt.preventDefault();
+  }
 
     render() {
         return (
           <div>
-            <form>
-              <div className="form-item">
-                <input type="text" required>
-                  Post Title
-                </input>
-              </div>
-
-              <div className="form-item">
-                <textarea
-                  id="post-message"
-                  type="text"
-                  placeholder="Post Message"
-                  name="event_description"
-                
-                  rows="8"
-                  cols="50"
-                ></textarea>
-              </div>
+            <form onSubmit={this.handleSubmit}>
+              <textarea
+              type="text"
+              placeholder="Write post here"
+              name="activity-feed-post"
+              onChange={this.handleChange}
+              rows="8"
+              cols="50"></textarea>
+            
             </form>
           </div>
         );
