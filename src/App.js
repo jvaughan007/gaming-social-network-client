3782e999c28c735fc9b2cdbd5e747e29cde8b934
Donovan Le <dtle647@gmail.com>
2020-12-30 17:39:30 -0600

    fixed some migration files and services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
     Route,
     Switch,
 } from 'react-router-dom';
+
 import styled from 'styled-components';
 import GlobalStyle from './globalStyle';
 import Home from './components/Home/Home';
@@ -14,10 +15,26 @@ import Login from './components/Login/Login';
 import Games from './components/Games/Games';
 import UserDashboard from './components/UserDashboard/UserDashboard';
 import Game from './components/Game/Game';
-import UserProfile from './components/UserProfile/UserProfile';
-import { SocketProvider } from './SocketProvider';
 
 const App = () => {
+    // useEffect(() => {
+    //   const socket = socketIOClient('ws://localhost:5000');
+    //   // socket.on('message', (data) => {
+    //   //   console.log(data);
+    //   // });
+    //   socket.emit('message', {
+    //     user_id: 1,
+    //     conv_id: 420,
+    //     msg: 'from react msg hi bye this working?'
+    //   });
+
+    //   socket.on('message', (msg) => {
+    //     console.log(msg);
+    //   });
+
+    //   return () => socket.disconnect();
+    // }, []);
+
     return (
         <>
             <Router>
@@ -36,7 +53,7 @@ const App = () => {
                         <Route exact path='/login'>
                             <Login></Login>
                         </Route>
-                        <Route exact path='/chat'>
+                        <Route exact path='/messages'>
                             <Chat />
                         </Route>
                         <Route exact path='/games'>
@@ -44,9 +61,6 @@ const App = () => {
                         </Route>
                         <Route exact path='/game/:id'>
                             <Game></Game>
-                        </Route>
-                        <Route exact path='/:username'>
-                            <UserProfile />
                         </Route>
                         <Route path='/404'>
                             <NotFound></NotFound>
@@ -62,19 +76,15 @@ const App = () => {
 const StyledWrapper = styled.div`
     width: 28.8rem;
     margin: 0 auto;
-
     @media (min-width: 576px) {
         width: 50rem;
     }
-
     @media (min-width: 768px) {
         width: 70rem;
     }
-
     @media (min-width: 992px) {
         width: 90rem;
     }
-
     @media (min-width: 1200px) {
         width: 112rem;
     }
--- src/components/Messages/Chat.js
+++ src/components/Messages/Chat.js
@@ -1,9 +1,26 @@
+import { useState, useEffect, useRef } from 'react';
+import socketIOClient from 'socket.io-client';
 import styled from 'styled-components';
 import VertNavBar from '../VerticalNavBar/VertNavBar';
-import React, { useState } from 'react';
+
+const socket = socketIOClient('https://gaming-social-network.herokuapp.com');
 
 const Chat = () => {
     const [text, setText] = useState('');
+    const [messages, setMessages] = useState([
+        { text: 'first message' },
+        { text: 'second message' },
+    ]);
+    const messageInput = useRef(null);
+
+    useEffect(() => {
+        socket.on('message', (msg) => {
+            setMessages([...messages, msg]);
+        });
+        messageInput.current.focus();
+
+        // return () => socket.disconnect();
+    }, [messages]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,7 +28,10 @@ const Chat = () => {
         if (!text.trim().length) {
             return;
         }
-        console.log(text);
+
+        socket.emit('message', text);
+        messageInput.current.value = '';
+        return setText('');
     };
 
     return (
@@ -22,10 +42,21 @@ const Chat = () => {
                     <VertNavBar />
                 </nav>
                 <h1>Messages</h1>
+                <StyledMessages>
+                    <ul>
+                        {messages.map((msg, idx) => (
+                            <li key={idx}>
+                                <span>User: </span>
+                                {msg.text}
+                            </li>
+                        ))}
+                    </ul>
+                </StyledMessages>
                 <form onSubmit={handleSubmit}>
                     <input
                         type='text'
                         onChange={(e) => setText(e.target.value)}
+                        ref={messageInput}
                     />
                     <button type='submit'>Submit</button>
                 </form>
@@ -34,22 +65,36 @@ const Chat = () => {
     );
 };
 
+const StyledMessages = styled.div`
+    background: #9453d3;
+    color: #fff;
+    ul {
+        li {
+            span {
+                font-weight: 600;
+            }
+            :not(:last-child) {
+                margin-bottom: 0.8rem;
+            }
+        }
+    }
+`;
+
 const StyledMain = styled.main`
     width: 28.8rem;
     margin: 0 auto;
-
+    form {
+        display: flex;
+    }
     @media (min-width: 576px) {
         width: 50rem;
     }
-
     @media (min-width: 768px) {
         width: 70rem;
     }
-
     @media (min-width: 992px) {
         width: 90rem;
     }
-
     @media (min-width: 1200px) {
         width: 112rem;
     }
@@ -62,12 +107,10 @@ const StyledMain = styled.main`
         left: 0;
         z-index: -1000;
     }
-
     nav {
         position: fixed;
         left: 0;
     }
-
     h1 {
         color: white;
         padding-top: 1rem;
