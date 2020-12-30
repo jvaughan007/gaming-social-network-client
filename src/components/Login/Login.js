0fa7f3bec5fc82afcc69c9997b8c22f926225c00
Dominic Faulring <domcodesjs@gmail.com>
2020-12-29 17:03:03 -0800

    darked bg images on login and signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,132 +1,131 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import LoginForm from "./LoginForm";
-import desktopImage from "./images/login-photo.jpeg";
-import mobileImage from "./images/pexels.jpg";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import LoginForm from './LoginForm';
+import desktopImage from './images/login-photo.jpeg';
+import mobileImage from './images/pexels.jpg';
 
 const Login = () => {
-    return (
-        <>
-            <StyledMain>
-                <div className='login-image'></div>
-                <div className='login-page'>
-                    <div>
-                        <h1>
-                            Logging into the best gaming community in the world!
-                        </h1>
-                        <ul>
-                            <li>
-                                <span>-</span>Create your own gamer profile
-                            </li>
-                            <li>
-                                <span>-</span>Find and connect with gaming
-                                groups
-                            </li>
-                            <li>
-                                <span>-</span>Connect with gamers that share
-                                your
-                            </li>
-                            <li>
-                                <span>-</span>Share your gaming accomplishments
-                                with the world
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <LoginForm></LoginForm>
-                        <Link to='/signup'>
-                            Don't have an account? <span>Signup here</span>
-                        </Link>
-                    </div>
-                </div>
-            </StyledMain>
-        </>
-    );
+  return (
+    <>
+      <StyledMain>
+        <div className='login-image'></div>
+        <div className='login-page'>
+          <div>
+            <h1>Logging into the best gaming community in the world!</h1>
+            <ul>
+              <li>
+                <span>-</span>Create your own gamer profile
+              </li>
+              <li>
+                <span>-</span>Find and connect with gaming groups
+              </li>
+              <li>
+                <span>-</span>Connect with gamers that share your
+              </li>
+              <li>
+                <span>-</span>Share your gaming accomplishments with the world
+              </li>
+            </ul>
+          </div>
+          <div>
+            <LoginForm></LoginForm>
+            <Link to='/signup'>
+              Don't have an account? <span>Signup here</span>
+            </Link>
+          </div>
+        </div>
+      </StyledMain>
+    </>
+  );
 };
 
 const StyledMain = styled.main`
+  .login-image {
+    background-image: url(${mobileImage});
+    background-size: cover;
+    background-position: 0rem 0rem;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1000;
+    background-color: inherit;
+    opacity: 0.2;
+  }
+
+  .login-page {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    margin-top: 8rem;
+    grid-gap: 3.2rem;
+
+    a {
+      display: block;
+      margin-top: 3.2rem;
+      color: #fff;
+      text-align: center;
+      cursor: pointer;
+      font-weight: 700;
+
+      span {
+        color: #9453d3;
+      }
+    }
+
+    div {
+      width: 100%;
+    }
+
+    div:first-child {
+      h1 {
+        color: #fff;
+        margin-bottom: 2.4rem;
+        font-size: 2.4rem;
+      }
+
+      ul {
+        color: #fff;
+
+        li {
+          margin-bottom: 0.8rem;
+
+          span {
+            margin-right: 0.8rem;
+            border-radius: 100%;
+          }
+        }
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
     .login-image {
-        background-image: url(${mobileImage});
-        background-size: cover;
-        background-position: 0rem 0rem;
-        width: 100vw;
-        height: 100vh;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1000;
+      background-image: url(${desktopImage});
+      background-size: cover;
+      background-position: 0rem 0rem;
+      width: 100vw;
+      height: 100vh;
+      position: absolute;
+      left: 0;
+      z-index: -1000;
     }
     .login-page {
-        display: grid;
-        grid-template-columns: repeat(1, 1fr);
-        margin-top: 8rem;
-        grid-gap: 3.2rem;
+      grid-template-columns: repeat(2, 1fr);
+      margin: 0;
+      height: 100vh;
+      width: 100%;
+      align-items: center;
+      justify-items: center;
+      grid-gap: 4.8rem;
 
-        a {
-            display: block;
-            margin-top: 3.2rem;
-            color: #fff;
-            text-align: center;
-            cursor: pointer;
-
-            span {
-                color: #9453d3;
-            }
+      div:first-child {
+        h1 {
+          font-size: 3.2rem;
         }
-
-        div {
-            width: 100%;
-        }
-
-        div:first-child {
-            h1 {
-                color: #fff;
-                margin-bottom: 2.4rem;
-                font-size: 2.4rem;
-            }
-
-            ul {
-                color: #fff;
-
-                li {
-                    margin-bottom: 0.8rem;
-
-                    span {
-                        margin-right: 0.8rem;
-                        border-radius: 100%;
-                    }
-                }
-            }
-        }
+      }
     }
-
-    @media (min-width: 768px) {
-        .login-image {
-            background-image: url(${desktopImage});
-            background-size: cover;
-            background-position: 0rem 0rem;
-            width: 100vw;
-            height: 100vh;
-            position: absolute;
-            left: 0;
-            z-index: -1000;
-        }
-        .login-page {
-            grid-template-columns: repeat(2, 1fr);
-            margin: 0;
-            height: 100vh;
-            width: 100%;
-            align-items: center;
-            justify-items: center;
-            grid-gap: 4.8rem;
-
-            div:first-child {
-                h1 {
-                    font-size: 3.2rem;
-                }
-            }
-        }
-    }
+  }
 `;
 
 export default Login;
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,129 +1,130 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import SignupForm from "./SignupForm";
-import mobileImage from "./images/takehiro.jpg";
-import desktopImage from "./images/ella-don.jpg";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import SignupForm from './SignupForm';
+import mobileImage from './images/takehiro.jpg';
+import desktopImage from './images/ella-don.jpg';
 
 const Signup = () => {
-    return (
-        <>
-            <StyledMain>
-                <div className='signup-image'></div>
-                <div className='signup-page'>
-                    <div>
-                        <h1>Join the best gaming community in the world!</h1>
-                        <ul>
-                            <li>
-                                <span>-</span>Create your own gamer profile
-                            </li>
-                            <li>
-                                <span>-</span>Find and connect with gaming
-                                groups
-                            </li>
-                            <li>
-                                <span>-</span>Connect with gamers that share
-                                your
-                            </li>
-                            <li>
-                                <span>-</span>Share your gaming accomplishments
-                                with the world
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <SignupForm></SignupForm>
-                        <Link to='/login'>
-                            Already have an account? <span>Login here</span>
-                        </Link>
-                    </div>
-                </div>
-            </StyledMain>
-        </>
-    );
+  return (
+    <>
+      <StyledMain>
+        <div className='signup-image'></div>
+        <div className='signup-page'>
+          <div>
+            <h1>Join the best gaming community in the world!</h1>
+            <ul>
+              <li>
+                <span>-</span>Create your own gamer profile
+              </li>
+              <li>
+                <span>-</span>Find and connect with gaming groups
+              </li>
+              <li>
+                <span>-</span>Connect with gamers that share your
+              </li>
+              <li>
+                <span>-</span>Share your gaming accomplishments with the world
+              </li>
+            </ul>
+          </div>
+          <div>
+            <SignupForm></SignupForm>
+            <Link to='/login'>
+              Already have an account? <span>Login here</span>
+            </Link>
+          </div>
+        </div>
+      </StyledMain>
+    </>
+  );
 };
 
 const StyledMain = styled.main`
+  color: #fff;
+
+  .signup-image {
+    background-image: url(${mobileImage});
+    background-size: cover;
+    background-position: -8rem 0rem;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1000;
+    background-color: inherit;
+    opacity: 0.2;
+  }
+
+  .signup-page {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    margin-top: 8rem;
+    grid-gap: 3.2rem;
+
+    a {
+      display: block;
+      margin-top: 3.2rem;
+      text-align: center;
+      cursor: pointer;
+      font-weight: 700;
+
+      span {
+        color: #9453d3;
+      }
+    }
+
+    div {
+      width: 100%;
+    }
+
+    div:first-child {
+      h1 {
+        margin-bottom: 2.4rem;
+        font-size: 2.4rem;
+      }
+
+      ul {
+        li {
+          margin-bottom: 0.8rem;
+
+          span {
+            margin-right: 0.8rem;
+            border-radius: 100%;
+          }
+        }
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
     .signup-image {
-        background-image: url(${mobileImage});
-        background-size: cover;
-        background-position: -8rem 0rem;
-        width: 100vw;
-        height: 100vh;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1000;
+      background-image: url(${desktopImage});
+      background-size: cover;
+      background-position: 0rem 0rem;
+      width: 100vw;
+      height: 100vh;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1000;
     }
 
     .signup-page {
-        display: grid;
-        grid-template-columns: repeat(1, 1fr);
-        margin-top: 8rem;
-        grid-gap: 3.2rem;
+      grid-template-columns: repeat(2, 1fr);
+      margin: 0;
+      height: 100vh;
+      align-items: center;
+      justify-items: center;
+      grid-gap: 4.8rem;
 
-        a {
-            display: block;
-            margin-top: 3.2rem;
-            text-align: center;
-            cursor: pointer;
-
-            span {
-                color: #9453d3;
-            }
+      div:first-child {
+        h1 {
+          font-size: 3.2rem;
         }
-
-        div {
-            width: 100%;
-        }
-
-        div:first-child {
-            h1 {
-                margin-bottom: 2.4rem;
-                font-size: 2.4rem;
-            }
-
-            ul {
-                li {
-                    margin-bottom: 0.8rem;
-
-                    span {
-                        margin-right: 0.8rem;
-                        border-radius: 100%;
-                    }
-                }
-            }
-        }
+      }
     }
-
-    @media (min-width: 768px) {
-        .signup-image {
-            background-image: url(${desktopImage});
-            background-size: cover;
-            background-position: 0rem 0rem;
-            width: 100vw;
-            height: 100vh;
-            position: absolute;
-            top: 0;
-            left: 0;
-            z-index: -1000;
-        }
-
-        .signup-page {
-            grid-template-columns: repeat(2, 1fr);
-            margin: 0;
-            height: 100vh;
-            align-items: center;
-            justify-items: center;
-            grid-gap: 4.8rem;
-            color: #fff;
-
-            div:first-child {
-                h1 {
-                    font-size: 3.2rem;
-                }
-            }
-        }
-    }
+  }
 `;
 
 export default Signup;
