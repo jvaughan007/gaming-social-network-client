0e1a4b2f4bffcecd7b7482da0a06c3310c93a632
Donovan Le <dtle647@gmail.com>
2020-12-30 12:42:35 -0600

    added socket provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch,
 } from 'react-router-dom';
 import styled from 'styled-components';
 import GlobalStyle from './globalStyle';
@@ -14,65 +14,66 @@ import Login from './components/Login/Login';
 import Games from './components/Games/Games';
 import UserDashboard from './components/UserDashboard/UserDashboard';
 import Game from './components/Game/Game';
+import { SocketProvider } from './SocketProvider';
 
 const App = () => {
-  return (
-    <>
-      <Router>
-        <GlobalStyle></GlobalStyle>
-        <StyledWrapper>
-          <Switch>
-            <Route exact path='/'>
-              <Home></Home>
-            </Route>
-            <Route exact path='/dashboard'>
-              <UserDashboard></UserDashboard>
-            </Route>
-            <Route exact path='/signup'>
-              <Signup></Signup>
-            </Route>
-            <Route exact path='/login'>
-              <Login></Login>
-            </Route>
-            <Route exact path='/messages'>
-              <Chat />
-            </Route>
-            <Route exact path='/games'>
-              <Games></Games>
-            </Route>
-            <Route exact path='/game/:id'>
-              <Game></Game>
-            </Route>
-            <Route path='/404'>
-              <NotFound></NotFound>
-            </Route>
-            <Redirect to='/404' />
-          </Switch>
-        </StyledWrapper>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <GlobalStyle></GlobalStyle>
+                <StyledWrapper>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home></Home>
+                        </Route>
+                        <Route exact path='/dashboard'>
+                            <UserDashboard></UserDashboard>
+                        </Route>
+                        <Route exact path='/signup'>
+                            <Signup></Signup>
+                        </Route>
+                        <Route exact path='/login'>
+                            <Login></Login>
+                        </Route>
+                        <Route exact path='/messages'>
+                            <Chat />
+                        </Route>
+                        <Route exact path='/games'>
+                            <Games></Games>
+                        </Route>
+                        <Route exact path='/game/:id'>
+                            <Game></Game>
+                        </Route>
+                        <Route path='/404'>
+                            <NotFound></NotFound>
+                        </Route>
+                        <Redirect to='/404' />
+                    </Switch>
+                </StyledWrapper>
+            </Router>
+        </>
+    );
 };
 
 const StyledWrapper = styled.div`
-  width: 28.8rem;
-  margin: 0 auto;
+    width: 28.8rem;
+    margin: 0 auto;
 
-  @media (min-width: 576px) {
-    width: 50rem;
-  }
+    @media (min-width: 576px) {
+        width: 50rem;
+    }
 
-  @media (min-width: 768px) {
-    width: 70rem;
-  }
+    @media (min-width: 768px) {
+        width: 70rem;
+    }
 
-  @media (min-width: 992px) {
-    width: 90rem;
-  }
+    @media (min-width: 992px) {
+        width: 90rem;
+    }
 
-  @media (min-width: 1200px) {
-    width: 112rem;
-  }
+    @media (min-width: 1200px) {
+        width: 112rem;
+    }
 `;
 
 export default App;
--- src/SocketProvider.js
+++ src/SocketProvider.js
@@ -1,1 +1,26 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
+
+const SocketContext = React.createContext();
+
+export function useSocket() {
+    return useContext(SocketContext);
+}
+
+export function SocketProvider({ id, children }) {
+    const [socket, setSocket] = useState();
+
+    useEffect(() => {
+        const newSocket = io('http://localhost:5000', { query: { id } });
+        setSocket(newSocket);
+
+        return () => newSocket.close();
+    }, [id]);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+}
