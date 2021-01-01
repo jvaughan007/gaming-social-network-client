22be473624e832fb46fa17be10398ef1915f3d4b
Donovan Le <dtle647@gmail.com>
2021-01-01 13:24:16 -0600

    fixed mobile profile

--- src/App.js
+++ src/App.js
@@ -13,82 +13,20 @@ import Chat from './components/Chat/Chat';
 import Login from './components/Login/Login';
 import Games from './components/Games/Games';
 import UserDashboard from './components/UserDashboard/UserDashboard';
+import UserProfile from './components/UserProfile/UserProfile';
 import Game from './components/Game/Game';
 
 const App = () => {
-  // useEffect(() => {
-  //   const socket = socketIOClient('ws://localhost:5000');
-  //   // socket.on('message', (data) => {
-  //   //   console.log(data);
-  //   // });
-  //   socket.emit('message', {
-  //     user_id: 1,
-  //     conv_id: 420,
-  //     msg: 'from react msg hi bye this working?'
-  //   });
-
-  //   socket.on('message', (msg) => {
-  //     console.log(msg);
-  //   });
-
-  //   return () => socket.disconnect();
-  // }, []);
-
-  const theme = {
-    colors: {
-      dark1: '#131B21',
-      dark2: '#192229',
-      dark3: '#1F2731',
-      purple: '#9453D3',
-      teal: '#0D7377',
-      blue: '#14FFEC',
-      white: '#fff'
-    }
-  };
-
-  return (
-    <>
-      <Router>
-        <GlobalStyle></GlobalStyle>
-        <ThemeProvider theme={theme}>
-          <StyledWrapper>
-            <Switch>
-              <Route exact path='/'>
-                <Home></Home>
-              </Route>
-              <Route exact path='/dashboard'>
-                <UserDashboard></UserDashboard>
-              </Route>
-              <Route exact path='/signup'>
-                <Signup></Signup>
-              </Route>
-              <Route exact path='/login'>
-                <Login></Login>
-              </Route>
-              <Route exact path='/chats'>
-                <Chat />
-              </Route>
-              <Route exact path='/games'>
-                <Games></Games>
-              </Route>
-              <Route exact path='/game/:id'>
-                <Game></Game>
-              </Route>
-              <Route path='/404'>
-                <NotFound></NotFound>
-              </Route>
-              <Redirect to='/404' />
-            </Switch>
-          </StyledWrapper>
-        </ThemeProvider>
-      </Router>
-    </>
-  );
-};
-
-const StyledWrapper = styled.div`
-  width: 28.8rem;
-  margin: 0 auto;
+    // useEffect(() => {
+    //   const socket = socketIOClient('ws://localhost:5000');
+    //   // socket.on('message', (data) => {
+    //   //   console.log(data);
+    //   // });
+    //   socket.emit('message', {
+    //     user_id: 1,
+    //     conv_id: 420,
+    //     msg: 'from react msg hi bye this working?'
+    //   });
 
     //   socket.on('message', (msg) => {
     //     console.log(msg);
@@ -97,39 +35,56 @@ const StyledWrapper = styled.div`
     //   return () => socket.disconnect();
     // }, []);
 
+    const theme = {
+        colors: {
+            dark1: '#131B21',
+            dark2: '#192229',
+            dark3: '#1F2731',
+            purple: '#9453D3',
+            teal: '#0D7377',
+            blue: '#14FFEC',
+            white: '#fff',
+        },
+    };
+
     return (
         <>
             <Router>
                 <GlobalStyle></GlobalStyle>
-                <StyledWrapper>
-                    <Switch>
-                        <Route exact path='/'>
-                            <Home></Home>
-                        </Route>
-                        <Route exact path='/dashboard'>
-                            <UserDashboard></UserDashboard>
-                        </Route>
-                        <Route exact path='/signup'>
-                            <Signup></Signup>
-                        </Route>
-                        <Route exact path='/login'>
-                            <Login></Login>
-                        </Route>
-                        <Route exact path='/messages'>
-                            <Chat />
-                        </Route>
-                        <Route exact path='/games'>
-                            <Games></Games>
-                        </Route>
-                        <Route exact path='/game/:id'>
-                            <Game></Game>
-                        </Route>
-                        <Route path='/404'>
-                            <NotFound></NotFound>
-                        </Route>
-                        <Redirect to='/404' />
-                    </Switch>
-                </StyledWrapper>
+                <ThemeProvider theme={theme}>
+                    <StyledWrapper>
+                        <Switch>
+                            <Route exact path='/'>
+                                <Home></Home>
+                            </Route>
+                            <Route exact path='/dashboard'>
+                                <UserDashboard></UserDashboard>
+                            </Route>
+                            <Route exact path='/signup'>
+                                <Signup></Signup>
+                            </Route>
+                            <Route exact path='/login'>
+                                <Login></Login>
+                            </Route>
+                            <Route exact path='/chats'>
+                                <Chat />
+                            </Route>
+                            <Route exact path='/:username'>
+                                <UserProfile />
+                            </Route>
+                            <Route exact path='/games'>
+                                <Games></Games>
+                            </Route>
+                            <Route exact path='/game/:id'>
+                                <Game></Game>
+                            </Route>
+                            <Route path='/404'>
+                                <NotFound></NotFound>
+                            </Route>
+                            <Redirect to='/404' />
+                        </Switch>
+                    </StyledWrapper>
+                </ThemeProvider>
             </Router>
         </>
     );
