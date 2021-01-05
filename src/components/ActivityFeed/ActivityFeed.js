6d7d4bf8da487fb1efe5ec0abb5e42bdd73364f3
Dominic Faulring <domcodesjs@gmail.com>
2021-01-05 10:00:42 -0800

    seperated post

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -40,28 +40,29 @@ const ActivityFeed = () => {
       <CreatePost addPost={addPost} />
       <FeedContent>
         {posts.map((post) => (
-          <li key={post.entity_id}>
-            <img src={ellipseIcon} alt='More Options' className='ellipse' />
-            <div className='post-user-info'>
-              <img
-                src='https://gaming-social-network.s3-us-west-2.amazonaws.com/avatar_placeholder.png'
-                alt='Avatar'
-                className='avatar'
-              ></img>
-              <h3>{post.username}</h3>
-            </div>
-            <div className='post-content'>
-              <p>{post.post_text}</p>
-            </div>
-            <div className='user-interactions'>
-              <div>
-                <img src={likeIcon} alt='Like' />
-                <img src={commentIcon} alt='Comment' />
-              </div>
-            </div>
-          </li>
+          <ActivityFeedPost></ActivityFeedPost>
         ))}
-      </FeedContent>
+      </FeedContent>{' '}
+      <li key={post.entity_id}>
+        <img src={ellipseIcon} alt='More Options' className='ellipse' />
+        <div className='post-user-info'>
+          <img
+            src='https://gaming-social-network.s3-us-west-2.amazonaws.com/avatar_placeholder.png'
+            alt='Avatar'
+            className='avatar'
+          ></img>
+          <h3>{post.username}</h3>
+        </div>
+        <div className='post-content'>
+          <p>{post.post_text}</p>
+        </div>
+        <div className='user-interactions'>
+          <div>
+            <img src={likeIcon} alt='Like' />
+            <img src={commentIcon} alt='Comment' />
+          </div>
+        </div>
+      </li>
     </StyledFeed>
   ) : null;
 };
