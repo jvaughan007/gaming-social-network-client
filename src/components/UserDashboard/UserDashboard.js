bbb1fd76f4f97abc444083fca37f8f7a6bc8677f
Dominic Faulring <domcodesjs@gmail.com>
2021-01-07 10:18:35 -0800

    curr

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -2,29 +2,29 @@ import styled from 'styled-components';
 import ActivityFeed from '../ActivityFeed/ActivityFeed';
 
 const UserDashboard = () => {
-    return (
-        <StyledMain>
-            <h1>User's Dashboard</h1>
-            <ActivityFeed></ActivityFeed>
-        </StyledMain>
-    );
+  return (
+    <StyledMain>
+      <h1>User's Dashboard</h1>
+      <ActivityFeed></ActivityFeed>
+    </StyledMain>
+  );
 };
 
 const StyledMain = styled.main`
-    position: fixed;
-    overflow: auto;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #0d7377;
+  position: fixed;
+  overflow: auto;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #0d7377;
 
-    h1 {
-        color: white;
-        text-align: center;
-        margin: 5rem;
-        margin-bottom: 2rem;
-    }
+  h1 {
+    color: white;
+    text-align: center;
+    margin: 5rem;
+    margin-bottom: 2rem;
+  }
 `;
 
 export default UserDashboard;
