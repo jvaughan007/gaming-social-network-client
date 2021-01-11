ac19b58d39587eb641f27c172862e351259f2f4d
Dominic Faulring <domcodesjs@gmail.com>
2021-01-11 13:51:57 -0800

    create group working

--- src/components/CreateGroup/CreateGroupForm.js
+++ src/components/CreateGroup/CreateGroupForm.js
@@ -1,18 +1,40 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { API_URL } from '../../config';
 
 const CreateGroupForm = () => {
+  const [groupName, setGroupName] = useState('');
+  // const [bannerImage, setBannerImage] = useState(null);
+  const [errors, setErrors] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  let history = useHistory();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!groupName.trim().length) {
+      return;
+    }
+
     try {
-      const res = fetch(`${API_URL}/groups`, {
+      setErrors(null);
+      setProcessing(true);
+
+      // if (!bannerImage) {
+      //   setProcessing(false);
+      //   return setErrors(['You must provide an image']);
+      // }
+      const token = localStorage.getItem('jwt');
+
+      const res = await fetch(`${API_URL}/groups`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
         },
-        body: JSON.stringify('asdfasdf')
+        body: JSON.stringify({ group_name: groupName })
       });
       const data = await res.json();
 
@@ -21,17 +43,25 @@ const CreateGroupForm = () => {
         return;
       }
 
-      console.log(data);
-
-      // redirect here or something
+      setProcessing(false);
+      return history.push(`/group/${data.group.slug}`);
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    <StyledForm>
-      <input type='text' />
+    <StyledForm onSubmit={handleSubmit}>
+      <input
+        type='text'
+        onChange={(e) => setGroupName(e.target.value)}
+        required
+      />
+      {processing ? (
+        <button disabled>Create Group</button>
+      ) : (
+        <button type='submit'>Create Group</button>
+      )}
     </StyledForm>
   );
 };
