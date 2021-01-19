84184d98196e697c7ebcaece382a6ebaed8baea6
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-19 15:38:36 -0600

    started list of features

--- src/components/DemoPage/DemoPage.js
+++ src/components/DemoPage/DemoPage.js
@@ -2,8 +2,8 @@ import Component from 'react';
 import styled from 'styled-components';
 
 
-class DemoPage extends Component {
-    return (
+const DemoPage = () => {
+        return (
         <div>
             <StyledMain>
                 <div className="demoHeading"> 
@@ -13,12 +13,36 @@ class DemoPage extends Component {
                 </div>
                 <div className="demoGetStarted">
                 <h3>Signup</h3> 
-                <p>Upon reaching the home page, you will be prompted to log in or to create an account.</p>
+                <p>
+                    Upon reaching the home page, you will be prompted 
+                    to log in or to create an account. Here, you will input 
+                    all the required fields and, on submission, be 
+                    redirected to your dashboard!
+                </p>
+                <br />
+                <h3>Login</h3>
+                <p>
+                    If you alredy have an account, simply enter your email 
+                    and password to be redirected to your dashbard upon
+                    submission!
+                </p>
+                <br />
+                <h3>Dashboard</h3>
+                <p>Once you have reached your dashboard, you can...</p>
+                <ol>
+                    <li>Edit your profile! (i.e. - avatar, about, banner, etc.)</li>
+                    <li>Add a list of your favorite games!</li>
+                    <li>Find and connect with game groups!</li>
+                    <li>Add other gamers as friends!</li>
+                    <li>Share your accomplishments with others!</li>
+                    <li>Create, share and read posts to the community!</li>
+                </ol>
                 </div>
             </StyledMain>
         </div>
     );
 };
+
 
 const StyledMain = styled.main`
 
