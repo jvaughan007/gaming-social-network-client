96f0bda875fbfba2d1884a65fb533a82cb18129d
Donovan Le <dtle647@gmail.com>
2021-01-21 11:05:17 -0600

    added side bar to every component

--- src/components/CreateGroup/CreateGroup.js
+++ src/components/CreateGroup/CreateGroup.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 import CreateGroupForm from './CreateGroupForm';
+import SideBar from '../Sidebar/Sidebar';
 
 const CreateGroup = () => {
-  return (
-    <StyledMain>
-      <h1>Create Group</h1>
-      <CreateGroupForm></CreateGroupForm>
-    </StyledMain>
-  );
+    return (
+        <StyledMain>
+            <nav>
+                <SideBar />
+            </nav>
+            <h1>Create Group</h1>
+            <CreateGroupForm></CreateGroupForm>
+        </StyledMain>
+    );
 };
 
-const StyledMain = styled.main``;
+const StyledMain = styled.main`
+    nav {
+        position: fixed;
+        left: 0;
+        top: 0;
+    }
+`;
 
 export default CreateGroup;
