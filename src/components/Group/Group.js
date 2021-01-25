a40aaf00c3c31a45be5007eefb6240f3d0cb700a
Dominic Faulring <domcodesjs@gmail.com>
2021-01-25 10:57:29 -0800

    styled header group

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -4,6 +4,7 @@ import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Loader from 'react-loader-spinner';
 import styled from 'styled-components';
 import { API_URL } from '../../config';
+import Sidebar from '../Sidebar/Sidebar';
 
 // we need to get the group members
 // we need to get the group posts
@@ -116,40 +117,108 @@ const Group = () => {
 
     if (!group && !loading) {
       return (
-        <StyledNoGroupMain>
-          <h2 className='no-results'>This group does not exist 😞</h2>
-        </StyledNoGroupMain>
+        <StyledDiv>
+          <Sidebar></Sidebar>
+          <StyledNoGroupMain>
+            <h2>This group does not exist 😞</h2>
+          </StyledNoGroupMain>
+        </StyledDiv>
       );
     }
 
     return (
-      <StyledMain>
-        <h1 className='group-name'>{group.group_name}</h1>
-        {isMember ? (
-          <button onClick={handleLeaveGroup}>Leave Group</button>
-        ) : (
-          <button onClick={handleJoinGroup}>Join Group</button>
-        )}
-      </StyledMain>
+      <>
+        <Sidebar></Sidebar>
+        <StyledDiv>
+          <StyledHeader>
+            <div className='avatar-and-title'>
+              <img src={group.image_url} alt='Avatar' />
+              <h1 className='group-name'>{group.group_name}</h1>
+            </div>
+
+            {isMember ? (
+              <button onClick={handleLeaveGroup}>Leave Group</button>
+            ) : (
+              <button onClick={handleJoinGroup}>Join Group</button>
+            )}
+          </StyledHeader>
+          <StyledMain></StyledMain>
+        </StyledDiv>
+      </>
     );
   };
 
   return renderGroup();
 };
 
-const StyledNoGroupMain = styled.main`
-  .no-results {
-    margin-top: 4rem;
-    text-align: center;
+const StyledDiv = styled.div`
+  width: calc(100% - 20rem);
+  float: right;
+  @media all and (max-width: 970px) {
     width: 100%;
-    color: #14ffec;
   }
 `;
 
-const StyledMain = styled.main`
-  .group-name {
-    color: #fff;
+const StyledNoGroupMain = styled.main`
+  width: calc(100% - 20rem);
+  height: 100vh;
+  float: right;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h2 {
+    color: #14ffec;
+  }
+
+  @media all and (max-width: 970px) {
+    width: 100%;
   }
 `;
+
+const StyledHeader = styled.header`
+  height: 20vh;
+  background: url('https://image.freepik.com/free-vector/simple-unique-gaming-banner-template_92741-92.jpg')
+    no-repeat;
+  background-size: 100% 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  button {
+    border-radius: 0.4rem;
+    border: none;
+    cursor: pointer;
+    height: 4.8rem;
+    padding: 0.8rem 3.2rem;
+    background: #9453d3;
+    color: #fff;
+  }
+
+  .avatar-and-title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      width: 8.8rem;
+      height: 8.8rem;
+      border-radius: 100%;
+      border: 0.2rem solid #fff;
+    }
+
+    .group-name {
+      color: #fff;
+      background: #000;
+      opacity: 0.9;
+      margin: 1.6rem 0;
+      padding: 0.8rem;
+    }
+  }
+`;
+
+const StyledMain = styled.main``;
 
 export default Group;
