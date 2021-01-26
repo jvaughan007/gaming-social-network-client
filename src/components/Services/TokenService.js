3c297afd3c0f5e0af34d28178f970a0f1e04c16c
Dominic Faulring <domcodesjs@gmail.com>
2021-01-25 18:52:32 -0800

    replaced localhost with api url config var

--- src/components/Services/TokenService.js
+++ src/components/Services/TokenService.js
@@ -1,22 +1,21 @@
-import jwtDecode from 'jwt-decode';
+import { API_URL } from '../../config';
 
-//localhost/api/auth/verifyJWT
 const TokenService = {
-    async checkForToken() {
-        const jwt = window.localStorage.getItem('jwt');
+  async checkForToken() {
+    const jwt = window.localStorage.getItem('jwt');
 
-        const res = await fetch('http://localhost:5000/auth/verifyJWT', {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                authorization: `Bearer ${jwt}`,
-            },
-        });
+    const res = await fetch(`${API_URL}/auth/verifyJWT`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${jwt}`
+      }
+    });
 
-        const data = await res.json();
-        return data;
-    },
+    const data = await res.json();
+    return data;
+  }
 };
 
 export default TokenService;
