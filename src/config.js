c3cc784b95c452dc530461bd315e2901974f54d6
Dominic Faulring <domcodesjs@gmail.com>
2021-01-25 18:44:56 -0800

    deleted jwt thing

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
 export const API_URL = 'https://gaming-social-network.herokuapp.com';
-
-export const API_JWT_TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJkYXJpc3NoYXJyaXNAZ21haWwuY29tIiwidXNlcm5hbWUiOiJkYXJpc3N0aGVkZXYiLCJpYXQiOjE2MDk3ODc0MzF9.c1ockQcjSmFsa0-GqMr3yEvCfk404YFF--cTflPV-Qw';
