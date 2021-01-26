47fa61e56d8d8386412aa2e839442c1609793c1b
mismyl123 <mlalonde309@gmail.com>
2021-01-26 16:52:24 -0500

    not finished

--- src/components/DemoPage/DemoPage.js
+++ src/components/DemoPage/DemoPage.js
@@ -12,56 +12,134 @@ import register from './images/register.jpg';
 
 
 const DemoPage = () => {
-        return (
+    return (
         <div>
             <StyledMain>
-                <div className="demoHeading"> 
-                    <h1>Welcome to the Gaming Social Network!</h1> 
+                <div className="demoHeading">
+                    <h1>Welcome to the Gaming Social Network!</h1>
                     <br />
                     <h2>Connect Hard, Win Harder</h2>
-                    <img src = {homepage}/>
-                    
-                </div>
-                <div className="demoGetStarted">
-                <h3>Signup</h3> 
-                <p>
-                    Upon reaching the home page, you will be prompted 
-                    to log in or to create an account. Here, you will input 
-                    all the required fields and, on submission, be 
-                    redirected to your dashboard!
+                    <img src={homepage} />
 
-                </p>
-                <img src = {register}/>
-                <br />
-                <h3>Login</h3>
-                <p>
-                    If you already have an account, simply enter your email 
-                    and password to be redirected to your dashboard upon
-                    submission!
-                </p>
-                <img src = {login}/>
-                <br />
-                <h3>Dashboard</h3>
-                <p>Once you have reached your dashboard, you can...</p>
-                <ol>
-                    <li>Edit your profile! (i.e. - avatar, about, banner, etc.)</li>
-                    <img src = {profile}/>
-                    <li>Create and add to a list of your favorite games!</li>
-                    <img src = {games}/>
-                    <li>Find and connect with game groups!</li>
-                    <img src = {groups}/>
-                    <li>Add other gamers as friends!</li>
-                    <img src = {find_friends}/>
-                    <li>Share your accomplishments with others!</li>
-                    <img src = {dashboard}/>
-                    <li>Create, share and read posts to the community!</li>
-                </ol>
+                </div>
+
+
+
+                <div className="demoSectionContainer">
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                            <img src={register} />
+                        </div>
+
+                        <div className="demoDescription">
+                            <h2>Signup</h2>
+                            <p>
+                                Upon reaching the home page, you will be prompted
+                                to log in or to create an account. Here, you will input
+                                all the required fields and, on submission, be
+                                redirected to your dashboard!
+
+                            </p>
+                        </div>
+                    </div>
+
+
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                            <img src={login} />
+                        </div>
+                        <div className="demoDescription">
+                            <h2>Login</h2>
+                            <p>
+                                If you already have an account, simply enter your email
+                                and password to be redirected to your dashboard upon
+                                submission!
+                            </p>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                            <img src={profile} />
+                        </div>
+                        <div className="demoDescription">
+
+                            <h2>Dashboard</h2>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                            <img src={profile} />
+                        </div>
+                        <div className="demoDescription">
+                            <h3>Once you have reached your dashboard, you can...</h3>
+
+                            <p>Edit your profile! (i.e. - avatar, about, banner, etc.)</p>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                            <img src={games} />
+                        </div>
+                        <div className="demoDescription">
+                            <p>Create and add to a list of your favorite games!</p>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                            <img src={groups} />
+                        </div>
+                        <div className="demoDescription">
+                        <p>Find and connect with game groups!</p>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                        <img src={find_friends} />
+                        </div>
+                        <div className="demoDescription">
+                            <p>Add other gamers as friends!</p>
+                        </div>
+                        </div>
+
+                    <br />
+
+                    <div className="demoSection">
+                        <div className="demoImage">
+                        <img src={dashboard} />
+                        </div>
+                        <div className="demoDescription">
+                    <p>Share your accomplishments with others!</p>
+                    <p>Create, share and read posts to the community!</p>
+                    </div>
+                    </div>
+
                     <div>
                         <Link to="/"><button>Let's Go!</button></Link>
                     </div>
                 </div>
+
             </StyledMain>
-        </div>
+        </div >
+
     );
 };
 
@@ -71,22 +149,38 @@ const StyledMain = styled.main`
         display: flex;
         flex-direction: column;
         margin-top: 2rem;
+
     }
 
     .demoHeading {
-        background-color: aliceblue;
+        background - color: aliceblue;
         font-size: small;
         text-align: center;
     }
 
-    .demoGetStarted {
-        text-align: right;
+    .demoSectionContainer {
         margin: 1rem .5rem 0 .5rem;
         background-color: aliceblue;
         padding: 1rem;
         margin-bottom: 2rem;
 
+    .demoSection {
+
     }
+
+    .demoImage {
+
+    }
+
+    .demoDescription {
+
+    }
+
+
+    }
+   
+    
+   
 `;
 
 export default DemoPage;
