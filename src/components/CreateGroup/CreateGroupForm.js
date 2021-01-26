d6dfb2843cf6b02a4d9dc4ec3898a617d185d48e
Dominic Faulring <domcodesjs@gmail.com>
2021-01-26 13:01:41 -0800

    groups responsive again

--- src/components/CreateGroup/CreateGroupForm.js
+++ src/components/CreateGroup/CreateGroupForm.js
@@ -41,8 +41,9 @@ const CreateGroupForm = () => {
       const data = await res.json();
 
       if (!data.success) {
-        setProcessing(false);
-        return;
+        const errors = data.errors.map((err) => err.msg);
+        setErrors(errors);
+        return setProcessing(false);
       }
       setProcessing(false);
       return history.push(`/group/${data.group.slug}`);
@@ -65,9 +66,34 @@ const CreateGroupForm = () => {
           ))}
         </StyledErrors>
       ) : null}
-      <input type='text' onChange={(e) => setName(e.target.value)} />
-      <textarea type='text' onChange={(e) => setDescription(e.target.value)} />
-      <input type='file' onChange={fileChangedHandler} />
+      <label htmlFor='name'>
+        Group Name <span>(Max Length: 50 characters)</span>
+      </label>
+      <input
+        type='text'
+        onChange={(e) => setName(e.target.value)}
+        id='name'
+        maxLength='50'
+        required
+      />
+      <label htmlFor='description'>
+        Description <span>(Max Length: 140 characters)</span>
+      </label>
+      <textarea
+        type='text'
+        onChange={(e) => setDescription(e.target.value)}
+        id='description'
+        maxLength='140'
+        required
+      />
+      <label htmlFor='avatar'>Group Avatar</label>
+      <input type='file' onChange={fileChangedHandler} required id='avatar' />
+      {image ? (
+        <div className='avatar-preview'>
+          <h3>Avatar Preview</h3>
+          <img src={URL.createObjectURL(image)} alt='Avatar Preview' />
+        </div>
+      ) : null}
       {processing ? (
         <button disabled>Create Group</button>
       ) : (
@@ -81,15 +107,76 @@ const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   max-width: 68rem;
-  margin: 0 auto;
+  margin: 3.2rem auto 0 auto;
   padding: 0 1.6rem;
 
-  input {
+  label {
+    margin-bottom: 0.8rem;
+    font-weight: 700;
+    font-size: 1.8rem;
+
+    span {
+      font-size: 1.4rem;
+      font-weight: 400;
+    }
+  }
+
+  label[for='avatar'] {
+    margin-top: 1.6rem;
+  }
+
+  input[type='text'] {
     height: 4.8rem;
     border-radius: 0.4rem;
     padding-left: 0.8rem;
     border: none;
     outline: none;
+    margin-bottom: 1.6rem;
+  }
+
+  input[type='file'] {
+    cursor: pointer;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .avatar-preview {
+    margin: 1.6rem auto 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h3 {
+      font-size: 1.8rem;
+      font-weight: 500;
+      margin-bottom: 0.8rem;
+    }
+
+    img {
+      width: 8.8rem;
+      height: 8.8rem;
+      border-radius: 100%;
+      border: 0.2rem solid #fff;
+    }
+  }
+
+  textarea {
+    border-radius: 0.4rem;
+    padding: 0.8rem;
+    border: none;
+    outline: none;
+    height: 9.6rem;
+  }
+
+  button {
+    margin-top: 1.6rem;
+    height: 4.8rem;
+    cursor: pointer;
+    background: #9453d3;
+    color: #fff;
+    outline: none;
+    border: none;
+    border-radius: 0.4rem;
   }
 
   @media all and (min-width: 970px) {
