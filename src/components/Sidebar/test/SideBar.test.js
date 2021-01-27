541b6c29b752bf7234034687f125542e36380ead
Dominic Faulring <domcodesjs@gmail.com>
2021-01-27 10:28:31 -0800

    lowercased test file imports

--- src/components/Sidebar/test/SideBar.test.js
+++ src/components/Sidebar/test/SideBar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SideBar from '../SideBar';
+import Sidebar from '../Sidebar';
 
 it('SideBar component renders without crashing', () => {
-  shallow(<SideBar></SideBar>);
+  shallow(<Sidebar></Sidebar>);
 });
